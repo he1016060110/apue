57786fc51af9dfb9721521cdc6560a79e00eb21d
hexi <1546151406@qq.com>
2018-04-22 11:26:02 +0800

    test getenv3

---
+++ threadctl/test_getenv3.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include "getenv3.c"
+
+void * thr_fn(void * arg)
+{
+  char * name = (char *) (arg);
+  int i = 0;
+  for(;;) {
+    char * env_name = getenv(name);  
+    printf("%s\n", env_name);
+    i++;
+    if (i > 100000) {
+      break;
+    }
+  }
+  return (void *) NULL;
+}
+int main()
+{
+  pthread_t tid;
+  int err;
+  int count = 0;
+  const char * envname;
+  for(; count < 20; count++) {
+    envname = count % 2 == 0? "PATH" :"CMAKE_CXX_COMPILER";
+    err = pthread_create(&tid,NULL, thr_fn, (void *)envname);  
+    if (err != 0 ) {
+      printf("error!%d", err);
+    }
+  }
+  sleep(3);
+}
