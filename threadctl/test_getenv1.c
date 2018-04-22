4ca74d5be87659e23d3c6f346f6455a4cf367c4b
hexi <1546151406@qq.com>
2018-04-22 11:06:22 +0800

    different path name

--- threadctl/test_getenv1.c
+++ threadctl/test_getenv1.c
@@ -4,10 +4,15 @@
 
 void * thr_fn(void * arg)
 {
-  char * name = "PATH";
+  char * name = (char *) (arg);
+  int i = 0;
   for(;;) {
     char * env_name = getenv(name);  
     printf("%s\n", env_name);
+    i++;
+    if (i > 100000) {
+      break;
+    }
   }
   return (void *) NULL;
 }
@@ -16,8 +21,10 @@ int main()
   pthread_t tid;
   int err;
   int count = 0;
-  for(; count < 100; count++) {
-    err = pthread_create(&tid,NULL, thr_fn, (void *)NULL);  
+  const char * envname;
+  for(; count < 20; count++) {
+    envname = count % 2 == 0? "PATH" :"CMAKE_CXX_COMPILER";
+    err = pthread_create(&tid,NULL, thr_fn, (void *)envname);  
     if (err != 0 ) {
       printf("error!%d", err);
     }
